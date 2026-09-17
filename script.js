b26099bc18d7e09f2c5d40441a75e34aae054d8f
jacs121 <101277107+jacs121@users.noreply.github.com>
2026-09-17 17:11:53 +0300

    Fix image paths and update hint message in script.js

--- script.js
+++ script.js
@@ -7,28 +7,27 @@ var hintMessage
 if (key === null) { // when entering the page
     hintMessage = "add '?key=' then a secret key to the end of the URL to START"
     image.title = "webers"
-    image.src = "images/icon.png"
+    image.src = "./images/icon.png"
 } else {
-    
     let progress = Number(localStorage.getItem("progress") ?? 0);
-    
+
     const levels = {
         webers: {
             index: 1,
             description: "what a musical me, though hearing it is oddly",
-            image: "images/clock.png",
+            image: "./images/clock.png",
             title: "clock",
             hint: "sorry for the shameless plug"
         },
-    
+        
         SuddenTimes: {
             index: 2,
             description: "why does 7 hate 5?",
-            image: "image/light_bulb.png",
+            image: "./images/light_bulb.png",
             title: "light_bulb",
             hint: "an idea"
         },
-    
+        
         light6bulb: {
             index: 3,
             description: "",
@@ -37,11 +36,12 @@ if (key === null) { // when entering the page
             hint: ""
         }
     };
+    console.log(levels[key]);
     
     const redHerrings = {
         start: {
             description: "you need to do better than that to get to level 1",
-            image: "images/icon.png",
+            image: "./images/icon.png",
             title: "webers",
             hint: "no really, welcome"
         },
@@ -62,8 +62,9 @@ if (key === null) { // when entering the page
     
         if (page.index > progress + 1) {
             descriptionInfo.textContent = "nice try :)";
+            hintMessage = "if you didn't realize it, you cant do that"
     
-            image.src = "images/icon.png";
+            image.src = "./images/icon.png";
             image.title = "webers";
         }
         else {
@@ -71,8 +72,7 @@ if (key === null) { // when entering the page
             image.src = page.image;
             image.title = page.title;
             hintMessage = page.hint;
-    
-            // affect progression.
+
             if (page.index === progress + 1) {
                 progress = page.index;
                 localStorage.setItem("progress", progress);
