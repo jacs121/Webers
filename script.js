c867cc5e0e7b15422c9feca310b5274d3230a094
jacs121 <101277107+jacs121@users.noreply.github.com>
2026-09-17 16:16:57 +0300

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ const descriptionInfo = document.getElementById("description")
 const image = document.getElementById("image")
 const key = params.get("key");
 
-if (key === null) {
+if (key === null) { // when entering the page
     var hintMessage = "add '?key=' then a secret key to the end of the URL to START"
 }
 
-if (key === "start") {
+if (key === "start") { // red haring
     descriptionInfo.textContent = "you need to do better then that to get to level1"
     hintMessage = "no really, welcome"
 }
 
-if (key === "webers") {
+if (key === "webers") { // the name of the puzzle
     descriptionInfo.textContent = "what a musical me, though hearing it is oddly"
-    image.src = "clock.png"
+    image.src = "images/clock.png"
     hintMessage = "sorry for the shameless plug"
 }
 
-if (key === "SuddenTimes") {
-    
+if (key === "SuddenTimes") { // a song I made in a album named oddly
+    descriptionInfo.textContent = "what a musical me, though hearing it is oddly"
+    image.src = "clock.png"
+    hintMessage = "sorry for the shameless plug"
 }
